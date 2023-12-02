6d315e7c6ab6f9dbbc12235ea5e7ef7df48d8cc3
Alex Lopez <aloro2392@gmail.com>
2023-12-02 09:33:01 +0100

    add utility to execute all days with a single method

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 import day1.Day1;
+import utils.Utils;
 
 public class Main {
     public static void main(String[] args) {
-        var day1 = new Day1();
-        day1.test();
-        day1.solve();
+        Utils.executeAllDays(1);
     }
 }
--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -1,6 +1,11 @@
 package utils;
 
+import java.util.Arrays;
+
 public class Utils {
+    private static int[] daysWithTestB = new int[] {
+            1
+    };
     public static <T> T cast(Object value, Class<T> type) {
         if (value instanceof Integer) {
             if (String.class.equals(type)) return type.cast(value.toString());
@@ -23,5 +28,27 @@ public class Utils {
         final double durationSeconds = durationMilli / 1_000.0;
         System.out.printf("Execution time: %dns, %dµs, %dms, %.2fs%n", durationNanos, durationMicro, durationMilli, durationSeconds);
         return result;
+    }
+
+    public static void executeDay(final int day, boolean hasTestB) {
+        try {
+            Class<Problem> problem = (Class<Problem>) Class.forName(String.format("day%d.Day%d", day, day));
+            var problemInstance = problem.getDeclaredConstructor().newInstance();
+            problem.getMethod("test").invoke(problemInstance);
+            problem.getMethod("solve").invoke(problemInstance);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void executeAllDays(final int total) {
+        for (int i = 1; i <= total; i++) {
+            int finalI = i;
+            boolean hasTestB = false;
+            if (Arrays.stream(daysWithTestB).anyMatch(day -> day == finalI)) {
+                hasTestB = true;
+            }
+            executeDay(i, hasTestB);
+        }
     }
 }
