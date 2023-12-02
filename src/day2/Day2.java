f61dd441235f77a51c674115eb742f3092ce7920
Alex Lopez <aloro2392@gmail.com>
2023-12-02 13:58:55 +0100

    utilities to parse wip

--- src/day2/Day2.java
+++ src/day2/Day2.java
@@ -1,20 +1,26 @@
 package day2;
 
-import utils.Logger;
-import utils.Problem;
-import utils.ProblemType;
-import utils.TestResults;
+import utils.*;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 @TestResults(resultA = "8", resultB = "undefined")
 public class Day2 extends Problem {
+
+    private final static int MAX_RED = 12;
+    private final static int MAX_GREEN = 13;
+    private final static int MAX_BLUE = 14;
+
     public Day2() {
         super(2);
     }
 
     @Override
     protected String solveProblem(ArrayList<String> lines, ProblemType type) {
+        for (String line : lines) {
+
+        }
         return null;
     }
 }
--- src/utils/RegexUtility.java
+++ src/utils/RegexUtility.java
@@ -1,19 +1,38 @@
 package utils;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegexUtility {
 
     private final Pattern regex;
+    private final Matcher matcher;
     private final String input;
 
-    public RegexUtility(Pattern regex, final String input) {
-        this.regex = regex;
+    public RegexUtility(String regex, final String input) {
+        this.regex = Pattern.compile(regex);
         this.input = input;
+        this.matcher = this.regex.matcher(input);
     }
 
-    public Matcher getMatches() {
-        return this.regex.matcher(input);
+    public String group(int g) {
+        if (this.matcher.find()) {
+            return this.matcher.group(g);
+        }
+        return "";
+    }
+
+    public ArrayList<String> all() {
+        final ArrayList<String> matches = new ArrayList<>();
+        if (this.matcher.find()) {
+            final int groupCount = this.matcher.groupCount();
+            for (int i = 0; i < groupCount; i++) {
+                matches.add(this.matcher.group(i));
+            }
+        }
+        return matches;
     }
 }
