df7334d1772b9395fb0aa3307a0fc2a5652b5727
Alex Lopez <aloro2392@gmail.com>
2023-12-02 19:01:00 +0100

    parser ended

--- src/day2/Day2.java
+++ src/day2/Day2.java
@@ -3,7 +3,8 @@ package day2;
 import utils.*;
 
 import java.util.ArrayList;
-import java.util.stream.Collectors;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @TestResults(resultA = "8", resultB = "undefined")
 public class Day2 extends Problem {
@@ -20,7 +21,67 @@ public class Day2 extends Problem {
     protected String solveProblem(ArrayList<String> lines, ProblemType type) {
         for (String line : lines) {
 
+            final Info ref = new Info();
+            Parser.sep(":", line).forEach(s -> {
+                if (s.startsWith("Game")) {
+                    ref.game = Utils.cast(Parser.sep(" ", s).skip(1).findFirst().get(), Integer.class);
+                } else {
+                    // parseando la parte derecha, a partir de los :
+                    Parser.sep(";", s).forEach(group -> {
+                        Info.Group g = new Info.Group();
+
+                        Parser.sep(",", group).forEach(color -> {
+                            String[] numAndColor = color.trim().split(" ");
+                            int num = Utils.cast(numAndColor[0], Integer.class);
+                            String colorName = numAndColor[1];
+                            switch (colorName) {
+                                case "red" -> g.reds = num;
+                                case "blue" -> g.blues = num;
+                                case "green" -> g.greens = num;
+                            }
+                        });
+
+                        ref.groups.add(g);
+                    });
+                }
+            });
+
+            Logger.getInstance().log("Parsed " + ref);
         }
         return null;
+    }
+
+
+    class Info {
+        int game;
+        List<Group> groups = new ArrayList<>();
+
+        @Override
+        public String toString() {
+            return "Info{" +
+                    "game=" + game +
+                    ", groups=" + groups.toString() +
+                    '}';
+        }
+
+        static class Group {
+            int reds;
+            int blues;
+            int greens;
+            Group() {}
+            Group(int reds, int blues, int greens) {
+                this.reds = reds;
+                this.blues = blues;
+                this.greens = greens;
+            }
+            @Override
+            public String toString() {
+                return "Group{" +
+                        "reds=" + reds +
+                        ", blues=" + blues +
+                        ", greens=" + greens +
+                        '}';
+            }
+        }
     }
 }
--- src/utils/Parser.java
+++ src/utils/Parser.java
@@ -1,54 +1,21 @@
 package utils;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Stream;
 
 public class Parser {
 
-    private final String originalText;
-    private Map<String, Block> blocksHash = new HashMap<>();
-    private Block lastBlock;
-    public Parser(String text) {
-        this.originalText = text;
-        Block defaultBlock = new Block();
-        defaultBlock.add(text);
-        blocksHash.put("default", defaultBlock);
-        lastBlock = defaultBlock;
-    }
-    public Parser divideBy(final String delimiter, final String hashKey, final String fromHashKey) {
-        if (hashKey.equals("default")) {
-            throw new IllegalArgumentException("Cannot put duplicate default key");
-        } else if (hashKey.equals("last")) {
-            throw new IllegalArgumentException("Cannot save with last key");
-        }
-
-        Block b;
-        if (fromHashKey != null) {
-            if (fromHashKey.equals("last")) b = lastBlock;
-            else b = blocksHash.get(fromHashKey);
-        } else {
-            b = blocksHash.get("default");
-        }
-
-        for (String c : b.contentList) {
-            final String[] splitResult = c.split(delimiter);
-            Block newBlock = new Block();
-            for (String r : splitResult) {
-                newBlock.add(r);
-            }
-        }
-
-        blocksHash.put(hashKey, b);
+    final String text;
+    public Parser(final String text) {
+        this.text = text;
     }
 
-    static class Block {
-
-        List<String> contentList = new ArrayList<>();
-
-        public void add(String content) {
-            this.contentList.add(content);
-        }
+    public Stream<String> sep(String delimiter) {
+        return Arrays.stream(text.split(delimiter));
     }
+
+    public static Stream<String> sep(String delimiter, String text) {
+        return Arrays.stream(text.split(delimiter));
+    }
+
 }
